7d3dc130e95e84e14265442dc16ca632eab5c4e1
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2024-10-25 12:42:43 +0530

    chages

--- greetings/function.go
+++ greetings/function.go
@@ -3,12 +3,27 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"log"
 )
 
 func Hello(name string) (string, error) {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	if name == "" {
+		log.Println("Name cannot be empty")
 		return "", errors.New("Name cannot be empty")
 	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+}
+
+func main() {
+	message, err := Hello("daman")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println(message)
+
 }
--- hello/hello.go
+++ hello/hello.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("HDYD")
+	message, err := greetings.Hello("")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
 	fmt.Println(message)
 }
